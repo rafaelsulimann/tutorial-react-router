51806fff13e0c68415c6f1e75cb91a3ce6ee4ba4
Rafael Sulimann <sulimannrafael@gmail.com>
2023-01-30 20:39:56 -0300

    Project created

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <>
+      <header>
+        <Header />
+      </header>
+    </>
+  );
+}
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import './styles.scss';
+
+export default function Header() {
+    return (
+        <div className="container header-container">
+            <div className="header-title">
+                <h1>Sulimann</h1>
+            </div>
+        </div>
+    );
+}
