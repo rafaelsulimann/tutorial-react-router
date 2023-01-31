06bc6c54a34f9b899f06c01622fd72f9974e97b6
Rafael Sulimann <sulimannrafael@gmail.com>
2023-01-30 23:19:11 -0300

    Links without full page reload

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './styles.scss';
 
 export default function Header() {
@@ -5,6 +6,10 @@ export default function Header() {
         <div className="container header-container">
             <div className="header-title">
                 <h1>Sulimann</h1>
+            </div>
+            <div className='header-nav'>
+                <Link className="links" to="/produtos">Produtos</Link>
+                <Link className="links" to="/categorias">Categorias</Link>
             </div>
         </div>
     );
