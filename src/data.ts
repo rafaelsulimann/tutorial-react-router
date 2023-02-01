54be144371e3b3589c06226208a6a652ecf8a761
Rafael Sulimann <sulimannrafael@gmail.com>
2023-01-31 23:45:08 -0300

    Listing products

---
+++ src/data.ts
@@ -0,0 +1,31 @@
+let products = [
+    {
+      name: "Santa Monica",
+      number: 1995,
+      amount: "$10,800"
+    },
+    {
+      name: "Stankonia",
+      number: 2000,
+      amount: "$8,000"
+    },
+    {
+      name: "Ocean Avenue",
+      number: 2003,
+      amount: "$9,500"
+    },
+    {
+      name: "Tubthumper",
+      number: 1997,
+      amount: "$14,000"
+    },
+    {
+      name: "Wide Open Spaces",
+      number: 1998,
+      amount: "$4,600"
+    },
+  ];
+  
+  export function getProducts() {
+    return products;
+  }
--- src/routes/Produtos/index.tsx
+++ src/routes/Produtos/index.tsx
@@ -1,10 +1,29 @@
+import { Link } from "react-router-dom";
 import Header from "../../components/Header";
+import { getProducts } from "../../data";
 import "./styles.scss";
 
 export default function Produtos() {
+
+    let products = getProducts();
   return (
-      <section id="main-title">
-        <h2>Produtos</h2>
-      </section>
+    <div style={{ display: "flex" }}>
+    <nav
+      style={{
+        borderRight: "solid 1px",
+        padding: "1rem",
+      }}
+    >
+      {products.map((products) => (
+        <Link
+          style={{ display: "block", margin: "1rem 0" }}
+          to={`/produtos/${products.number}`}
+          key={products.number}
+        >
+          {products.name}
+        </Link>
+      ))}
+    </nav>
+  </div>
   );
 }
