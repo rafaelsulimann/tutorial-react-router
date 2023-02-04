04762c87eaf05d6ecedd595ea862c0677b456d7f
Rafael Sulimann <sulimannrafael@gmail.com>
2023-02-03 23:12:55 -0300

    Search Params

--- src/routes/Produtos/index.tsx
+++ src/routes/Produtos/index.tsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet, useSearchParams } from "react-router-dom";
 import Header from "../../components/Header";
 import { getProducts } from "../../data";
 import "./styles.scss";
@@ -6,6 +6,7 @@ import "./styles.scss";
 export default function Produtos() {
 
     let products = getProducts();
+    let [searchParams, setSearchParams] = useSearchParams();
   return (
     <div style={{ display: "flex" }}>
     <nav
@@ -14,14 +15,31 @@ export default function Produtos() {
         padding: "1rem",
       }}
     >
-      {products.map((products) => (
-        <Link
-          style={{ display: "block", margin: "1rem 0" }}
+      <input type="text" value={searchParams.get("name") || ""} onChange= {(event) => {
+        let name = event.target.value;
+        if(name){
+          setSearchParams({ name })
+        }else {
+          setSearchParams({})
+        }
+      }} />
+      {products
+      .filter((product) => {
+        let name = searchParams.get("name");
+        if(!name){
+          return true;
+        }
+        let productName = product.name.toLowerCase();
+        return productName.startsWith(name.toLowerCase());
+      })
+      .map((products) => (
+        <NavLink
+          className={({ isActive}) => (isActive ? "nav-link active" : "nav-link non-active")}
           to={`/produtos/${products.number}`}
           key={products.number}
         >
           {products.name}
-        </Link>
+        </NavLink>
       ))}
     </nav>
     <Outlet />
